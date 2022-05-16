f618325babb57f91432acdb0463a26c711cabe00
kethllen <kety_andrey@hotmail.com>
2022-05-16 16:11:38 -0300

    feat: entrada routers

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import userRouter from "./userRouter.js";
-import entradaRouter from "./obrasRouter.js";
-
+import obraRouter from "./obrasRouter.js";
+import entradaRouter from "./entradaRouter.js";
 
 const router = Router();
 router.use(userRouter);
+router.use(obraRouter);
 router.use(entradaRouter);
-
 export default router;
