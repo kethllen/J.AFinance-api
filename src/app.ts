05caf80f442051433b2f6f236afbdc2a04e9d9a6
kethllen <kety_andrey@hotmail.com>
2022-05-23 11:54:57 -0300

    deploy

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,9 @@ import cors from "cors";
 import express from "express";
 import "express-async-errors";
 import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
-import dotenv from "dotenv";
 import router from "../src/routers/index.js";
+import dotenv from "dotenv";
+
 
 dotenv.config();
 
